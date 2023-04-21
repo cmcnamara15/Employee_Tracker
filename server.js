b84481710b417fa3a833a5f792654dcc09fb00ed
Chris McNamara <cmcnamara1991@icloud.com>
2023-04-21 16:01:01 -0500

    Add inquire logic to main()

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+require("console.table");
+const db = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_db'
+});
 
 const questions = [
     {
@@ -6,4 +14,31 @@ const questions = [
     },
 ]
 
-function main()
+function main(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to choose?',
+            name: 'action',
+            choices: [
+                'view all departments',
+                'view all roles',
+                'view all employees',
+                'add a department',
+                'add a role',
+                'add an employee',
+                'update a employee role'
+            ]
+        }
+    ])
+    .then((answers)=> {
+        if(answers.action == 'view all departments'){
+            db.query('SELECT * FROM department;', (err, dataRes)=> {
+                console.table(dataRes)
+                main();
+            })
+        }
+    })
+}
+
+main();
