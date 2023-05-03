a9b13631892af91f7854d0599eb149a93190abee
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-03 10:47:57 -0500

    Continue to add comments and structure file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// Required packages and connection
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require("console.table");
+
 const db = mysql.createConnection({
     host:'localhost',
     user: 'root',
