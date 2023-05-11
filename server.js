c3a5651150e51a99a94e3cf68a8e0ac7b2616fb7
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-11 13:32:47 -0500

    Fix typos in text and switch quotes from single to double

--- server.js
+++ server.js
@@ -40,13 +40,13 @@ function addDepartment(){
     inquirer
         .prompt([
             {
-                type: 'input',
-                message: 'What department would you like to add?',
-                name: 'addDep'
+                type: "input",
+                message: "What department would you like to add?",
+                name: "addDep"
             },
         ])
         .then(answers => {
-            db.query('INSERT INTO DEPARTMENT (department_name) VALUES (?)', 
+            db.query("INSERT INTO DEPARTMENT (department_name) VALUES (?)", 
             [answers.addDep], (err, dataRes) => {
                 main();
             })
@@ -58,23 +58,23 @@ function addRole(){
     inquirer
         .prompt ([
             {
-                type: 'input',
-                message: 'What is the title of the role would you like to add?',
-                name: 'role'
+                type: "input",
+                message: "What is the title of the role would you like to add?",
+                name: "role"
             },
             {
-                type: 'input',
-                message: 'What is the annual salary for this role?',
-                name: 'salary'
+                type: "input",
+                message: "What is the annual salary for this role?",
+                name: "salary"
             },
             {
-                type: 'input',
-                message: 'What is the department ID?',
-                name: 'deptId'
+                type: "input",
+                message: "What is the department ID?",
+                name: "deptId"
             }
         ])
         .then(answers => {
-            db.query('INSERT INTO ROLE (title, salary, department_id) VALUES (?, ?, ?)', 
+            db.query("INSERT INTO ROLE (title, salary, department_id) VALUES (?, ?, ?)", 
             [answers.role, answers.salary, answers.deptId], (err, dataRes) => {
                 main();
             })
@@ -84,36 +84,36 @@ function addRole(){
 
 // Function to add a employee
 function addEmployee(){
-    db.query('SELECT * FROM ROLE', (err, data)=> {
+    db.query("SELECT * FROM ROLE", (err, data)=> {
         const roles = data.map(row => {
             return {name: row.title, value: row.id}
         })
         inquirer
         .prompt ([
             {
-                type: 'input',
-                message: 'What is the employees first name?',
-                name: 'firstName'
+                type: "input",
+                message: "What is the employees first name?",
+                name: "firstName"
             },
             {
-                type: 'input',
-                message: 'What is the employees last name?',
-                name: 'lastName'
+                type: "input",
+                message: "What is the employees last name?",
+                name: "lastName"
             },
             {
-                type: 'list',
-                message: 'Please enter new employee role ID',
-                name: 'roleId',
+                type: "list",
+                message: "What is the employee's role",
+                name: "roleId",
                 choices: roles
             },
             {
-                type: 'input',
-                message: 'What is the manager id? (if manager enter NULL)',
-                name: 'managerId'
+                type: "input",
+                message: "What is the manager id? (if manager enter NULL)",
+                name: "managerId"
             }
         ])
         .then(answers => {
-            db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', 
+            db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", 
             [answers.firstName, answers.lastName, answers.roleId, answers.managerId], (err, dataRes) => {
                 main();
             })
@@ -123,11 +123,11 @@ function addEmployee(){
 
 // Function to update a role
 function updateRole(){
-    db.query('SELECT * FROM employee', (err, data)=> {
+    db.query("SELECT * FROM employee", (err, data)=> {
         const employees = data.map(row => { 
             return {name: `${row.first_name} ${row.last_name}`, value: row.id}
         });
-    db.query('SELECT * FROM role', (err, data) => {
+    db.query("SELECT * FROM role", (err, data) => {
         const newRole = data.map(row => {
             return {name: row.title, value: row.id}
         });
@@ -135,21 +135,21 @@ function updateRole(){
     inquirer
         .prompt([
             {
-                type: 'list',
+                type: "list",
                 message: "Which employee's role would you like to update?",
-                name: 'employeeUpdate',
+                name: "employeeUpdate",
                 choices: employees
             },
             {
-                type: 'list',
-                message: 'What role would you like to assign the selected employee?',
-                name: 'newRole',
+                type: "list",
+                message: "What role would you like to assign the selected employee?",
+                name: "newRole",
                 choices: newRole
             }
         ])
         .then(answers => {
             console.log(answers)
-            db.query('UPDATE employee SET role_id = ? WHERE id = ?', [answers.newRole, answers.employeeUpdate], (err, data)=> {
+            db.query("UPDATE employee SET role_id = ? WHERE id = ?", [answers.newRole, answers.employeeUpdate], (err, data)=> {
                 main();
             })
         })
@@ -202,7 +202,7 @@ function main() {
             case "add a role":
                 addRole();
                 break;
-            case "add an employee":
+            case "add a employee":
                 addEmployee();
                 break;
             case "update a employee role":
